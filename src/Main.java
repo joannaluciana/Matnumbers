94ef73bca9609559a651ddfe4a28784d0de8e267
Joanna <joannaluciana>
2021-03-21 20:43:23 +0100

    Fisher Yates

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
 
 public class Main {
-
+	public static void main(String[] args){
+		
+		Matnumbers NewArray = new Matnumbers ();
+		
+		int[]ar= NewArray.makeArray(99);
+//		
+//		for (int s:ar) {
+//			 System.out.println (s);
+//		 }
+		
+		int[]ar2=NewArray.shuffleArray(ar);
+		
+		for (int s:ar) {
+			 System.out.println (s);
+		 }
+		
+}
 }
--- src/Matnumbers.java
+++ src/Matnumbers.java
@@ -1,4 +1,69 @@
+import java.lang.Object;
+import java.util.Random;
+
+
+
+
 
 public class Matnumbers {
+	
+private int arrlength;
 
+public Matnumbers() {
+	super();
+	
+	
 }
+	
+	 
+		 
+		public static int[] makeArray (int arrlength) {
+			
+		 int[] nuMbers = new int[arrlength];
+		 
+		 
+		 
+		 for (int i = 0; i < arrlength; i++) {
+			 nuMbers[i] = i+1;
+			
+	        }
+		 return nuMbers;
+		}
+		
+		
+	 
+	 public void printArray(int [] x) {
+	 
+	 
+	  for (int s:x) {
+		 System.out.println (s);
+	 }
+	  
+	 }
+	 
+	 
+	 public static int[] shuffleArray(int [] x) {
+		 
+		 
+
+Random ran = new Random();
+		 
+		 
+		 
+		 for (int i = 0; i <x.length; i++) {
+			 
+		 int ranPos = ran.nextInt(x.length);
+		 int temp = x[i];
+		 x[i]=x[ranPos];
+		 x[ranPos] = temp;
+		 }
+		 
+	 return x;
+	 
+	 }
+	 
+		  
+		 
+		}
+
+
